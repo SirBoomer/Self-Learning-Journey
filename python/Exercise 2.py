a0140c8b1eaf1898d37a64f8d21b4246766fe750
SirBoomer <captaincheng123@gmail.com>
2026-08-25 17:13:21 +0800

    Exercise 2

---
+++ python/Exercise 2.py
@@ -0,0 +1,11 @@
+def count_words(text):
+    words=text.split()
+    counts={}
+    for i in words:
+        if i not in counts:
+
+            counts[i]=1
+        else:
+            counts[i]+=1
+    return counts
+print(count_words("the cat the dog the bird"))
